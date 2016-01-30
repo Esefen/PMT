51a5e6043ebcc16de3a2c8bf286e1a185b0a1bbf
Esefen <romain.soual.pro@gmail.com>
2016-01-30 20:05:11 +0100

    basic slide + invulnerability frames

--- Unity Project/Assets/PMT/Scripts/PlayerBehavior.cs
+++ Unity Project/Assets/PMT/Scripts/PlayerBehavior.cs
@@ -10,19 +10,30 @@ public class PlayerBehavior : MonoBehaviour {
     public float JUMP_INPUT_MAX_LENGTH = 0.4f;
     public float JUMP_COOLDOWN_LENGTH = 0.5f;
     public float GRAVITY = 10.0f;
+    public float SLIDE_LENGTH = 1.5f;
+    public float INVULNERABILITY_LENGTH = 0.5f;
     float speed, jumpPower;
     bool inAir = true;
     bool apexReached = true;
     bool jumpCooldown = true;
     bool doubleJumpCooldown = true;
+    bool upKeyReleased = true;
     Vector3 forwardMovement, verticalMovement = Vector3.zero;
     float timerJumpPower, timerJumpCooldown, timerGravityKick;
+    bool sliding = false;
+    float slideTimer;
+    float timerInvulnerability;
 
     public float hitPoints = 100;
     float MAX_HIT_POINTS = 100;
     public Image hpBar;
     CameraBehavior cam;
 
+    public float playerLevel = 1;
+    public bool slideUnlocked = true;
+    public bool doubleJumpUnlocked = true;
+    public bool dashUnlocked = true;
+    public bool shieldUnlocked = true;
 
 	// Use this for initialization
 	void Start () {
@@ -39,6 +50,7 @@ public class PlayerBehavior : MonoBehaviour {
         {
             inflictDamage(5);
         }
+
         if (Input.GetAxis("Horizontal") != 0)
         {
             if (Input.GetAxis("Horizontal") < 0) speed -= BASE_SPEED / 30.0f;
@@ -47,6 +59,7 @@ public class PlayerBehavior : MonoBehaviour {
             cam.changeXOffset((speed - BASE_SPEED) / BASE_SPEED);
         }
         else speed = Mathf.Lerp(speed, BASE_SPEED, 0.1f);
+        if (Input.GetAxis("Vertical") <= 0) upKeyReleased = true;
 
         if (inAir)
         {
@@ -71,9 +84,10 @@ public class PlayerBehavior : MonoBehaviour {
             {
                 verticalMovement = Vector3.down * Mathf.Clamp(Mathf.Lerp(0, GRAVITY, Time.time - timerGravityKick), 0, GRAVITY);
             }
-            if (!doubleJumpCooldown && Input.GetAxis("Vertical") > 0)
+            if (upKeyReleased && doubleJumpUnlocked && !doubleJumpCooldown && Input.GetAxis("Vertical") > 0)
             {
                 doubleJumpCooldown = true;
+                apexReached = false;
                 timerJumpPower = Time.time;
             }
         }
@@ -89,19 +103,32 @@ public class PlayerBehavior : MonoBehaviour {
             // Jump
             if (!jumpCooldown && Input.GetAxis("Vertical") > 0)
             {
-                if (!inAir)
+                if (sliding)
                 {
-                    inAir = true;
-                    jumpCooldown = true;
-                    timerJumpPower = Time.time;
+                    sliding = false;
+                    cController.enabled = true;
                 }
+                inAir = true;
+                jumpCooldown = true;
+                upKeyReleased = false;
+                timerJumpPower = Time.time;
                 jumpPower = BASE_JUMP;
                 verticalMovement = Vector3.up * jumpPower;
             }
             // Slide
-            else if (Input.GetAxis("Vertical") < 0)
+            else if (!sliding)
             {
-
+                if (slideUnlocked && Input.GetAxis("Vertical") < 0)
+                {
+                    sliding = true;
+                    slideTimer = Time.time;
+                    cController.enabled = false;
+                }
+            }
+            else if (Time.time - slideTimer > SLIDE_LENGTH)
+            {
+                sliding = false;
+                cController.enabled = true;
             }
         }
         forwardMovement = Vector3.right * speed;
@@ -139,9 +166,12 @@ public class PlayerBehavior : MonoBehaviour {
 
     public void inflictDamage(float damageValue)
     {
-        hitPoints = Mathf.Clamp(hitPoints - damageValue, 0, MAX_HIT_POINTS);
-        refreshHitPoints();
-        cam.launchShake();
+        if (Time.time - timerInvulnerability > INVULNERABILITY_LENGTH)
+        {
+            hitPoints = Mathf.Clamp(hitPoints - damageValue, 0, MAX_HIT_POINTS);
+            refreshHitPoints();
+            cam.launchShake();
+        }
     }
 
     void refreshHitPoints()
