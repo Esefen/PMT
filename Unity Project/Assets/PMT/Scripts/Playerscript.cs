8e8c8254752c547afa247a7a597981d498dc90b9
Valentin <valentinduchj@gmail.com>
2016-01-30 21:04:56 +0100

    Function DoorCrossed a ajouté au player (ça permet de changer de salle ( attention à l'incrémentation) Function Door a ajouté aux portes de sortie Function DoorEntry a ajouté aux portes d'entrée Function Move Player ( il sert à bouger le joueur ) Function PlayerScript (inutile)

---
+++ Unity Project/Assets/PMT/Scripts/Playerscript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	/// <summary>
+	/// 1 - La vitesse de déplacement
+	/// </summary>
+	public Vector2 speed = new Vector2(50, 50);
+	public int NombreDoor;
+	// 2 - Stockage du mouvement
+	private Vector2 movement;
+
+	void Update()
+	{
+		// 3 - Récupérer les informations du clavier/manette
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		// 4 - Calcul du mouvement
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+
+	}
+
+	void FixedUpdate()
+	{
+		// 5 - Déplacement
+		GetComponent<Rigidbody2D>().velocity = movement;
+	}
+
+}
