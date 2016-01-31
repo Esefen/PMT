f355a862903c360857eac0a58ada580a37794e9c
v.rigitano <v.rigitano@student.isartdigital.com>
2016-01-31 10:19:55 +0100

    Fix bugs

---
+++ Unity Project/Assets/PMT/Scripts/DoNotDestroy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoNotDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Awake()
+	{DontDestroyOnLoad(transform.gameObject);}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
